3f3b628ca94bb0f350718cd30c4729fab7c889f3
Wendy Battaliu <wendybattaliu@wendys-mbp.lan>
2022-07-05 19:50:44 -0400

    Enter input

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("hello")
+print("Awesome program to do power of 2.")
+
+# Prompt user for a number
+print("Enter a number:")
+input = input()
+
+# Check that the input is valid
+if input==1:
+	print(True)
+else:   
+	print(False)
+
+# Calculate power of 2
+result = input
+
+# Print results
+print(result)
